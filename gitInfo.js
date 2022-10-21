59dbd70812b9ee34b40c723dc97d09eaf965df98
Noah Cain <noahcain.work@gmail.com>
2022-10-21 09:51:48 -0700

    initial commit

---
+++ gitInfo.js
@@ -0,0 +1,101 @@
+/*
+    For this section of the assessment you will be putting 
+    together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition 
+    of what Git is.
+*/
+
+//CODE HERE
+
+let gitDefinition = 'Git is a version control system that tracks changes in files and keeps snapshots as references to that point in the file. '
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition 
+    of what GitHub is.
+*/
+
+//CODE HERE
+
+let gitHubDefinition = 'GitHub is a version control manager where files of code are stored for self-use, as well as for collaborating with other developers so multiple people can work together on a project.'
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition 
+    of what 'git init' does.
+*/
+
+//CODE HERE
+
+let gitInitDefinition = 'git init creates an empty repository for code to be stored in to be later pushed to a VCM(Git).'
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition 
+    of what 'git clone' does.
+*/
+
+//CODE HERE
+
+let gitCloneDefinition = 'git clone points to an already existing repository and then makes a copy of that repository in another location.'
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition 
+    of what 'git status' does.
+*/
+
+//CODE HERE
+
+let gitStatusDefinition = 'git status shows the current state of the working directory and the staging area.'
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition 
+    of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+
+let gitAddDefinition = 'git add takes a change from the working directory and puts it in the staging area that will be commited.'
+let gitAddCode = 'git add .'
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition 
+    of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit 
+    using the message "initial commit".
+*/
+
+//CODE HERE
+
+let gitCommitDefinition = 'git commit takes the files that were added to the staging area and creates the snapshot of the added code at that time.'
+let gitCommitCode = 'git commit -m"initial commit"'
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition 
+    of what 'git push' does.
+*/
+
+//CODE HERE
+
+let gitPushDefinition = 'git push takes the commited files and sends them from the local repository to the remote repository'
---
+++ sdlc.js
@@ -0,0 +1,29 @@
+// In this section you will answer some work through 
+// problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development 
+    Lifecycle to be in their correct order.
+*/
+
+// ---------------Planning------------------
+// --------Analysis of Requirements---------
+// ----------------Design-------------------
+// ------------Implementation---------------
+// ----------Testing & Integration----------
+// ---------------Maintain------------------
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the 
+    Software Development Lifecycle and set the values of 
+    each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = "This is the second step, where one defines what the application is supposed to, as well as any needed features."
+const design = "This is the third step. It is used to show how the application is meant to work, and how user input changes the user experience."
+const implementation = "This is the fourth step. The application is written out."
+const testingAndIntegration = "This is the fifth step. Continuous testing is done to ensure that each part of the application is running smoothly with the other parts of the application."
+const maintain = "This is the sixth and final step. After the application is deployed, more testing is done to discover any bugs that have made their way in to production."
